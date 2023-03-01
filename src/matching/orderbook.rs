cc5e2a9756036d7a4adbb057e5c79f53fc3f6038
Jan Baumann <janbaumann@mbp-j.local>
2023-03-01 09:48:10 +0100

    matching functions and matching signals

--- src/matching/orderbook.rs
+++ src/matching/orderbook.rs
@@ -4,10 +4,10 @@ pub struct Orderbook<'a> {
     security: &'a Security,
     starting_price: i64,
     current_market_price: i64,
-    best_bid: i64,  // the lowest price a buy order gets placed
-    best_ask: i64,  // the hightest price a sell order gets placed
-    worst_bid: i64, // the highest price a buy order gets placed
-    worst_ask: i64, // the lowest price a sell order gets placed
+    best_bid: i64,
+    best_ask: i64,
+    worst_bid: i64,
+    worst_ask: i64,
     order_map: HashMap<i64, Order<'a>>,
     buy_at_market_orders: VecDeque<i64>,
     sell_at_market_orders: VecDeque<i64>,
@@ -37,14 +37,14 @@ impl <'a> Orderbook<'a> {
         }
     }
 
-    fn insert_order(&mut self, mut order: Order<'a>) -> Result<(i64, MatchingSignal), String> {
+    fn insert_order(&mut self, order: &mut Order<'a>) -> Result<(i64, MatchingSignal), String> {
         if order.amount <= 0 { return Err("Order amount must be greater than zero".to_string()); }
 
         let new_order_id: i64 = 1;
         order.order_id = new_order_id;
         let order_limit = order.order_limit;
         let is_buy_order = order.is_buy_order;
-        self.order_map.insert(new_order_id, order);
+        self.buy_at_market_orders.len();
 
         if let Some(limit) = order_limit {
             if limit <= 0 { return Err("Limit must be greater than zero".to_string()); }
@@ -56,7 +56,7 @@ impl <'a> Orderbook<'a> {
                     self.buy_limit_orders.push_front(queue);
                     self.number_buy_limit_orders += 1;
                     self.best_bid = limit;
-                    return Ok((new_order_id, MatchingSignal::NewLowestBid));
+                    return Ok((new_order_id, MatchingSignal::NewHighestBid),);
                 }
 
                 if limit < self.worst_bid {
@@ -78,7 +78,7 @@ impl <'a> Orderbook<'a> {
                     self.buy_limit_orders.insert(index.try_into().unwrap(), queue);
                 }
                 
-                return Ok((new_order_id, MatchingSignal::NoOperation));
+                return Ok((new_order_id, if index == 0 { MatchingSignal::NewHighestBid } else { MatchingSignal::NoOperation } ));
             }
 
             if limit < self.best_ask {
@@ -87,7 +87,7 @@ impl <'a> Orderbook<'a> {
                 self.sell_limit_orders.push_front(queue);
                 self.number_sell_limit_orders += 1;
                 self.best_ask = limit;
-                return Ok((new_order_id, MatchingSignal::NewHighestAsk));
+                return Ok((new_order_id, MatchingSignal::NewLowestAsk));
             }
 
             if limit > self.worst_ask {
@@ -108,16 +108,10 @@ impl <'a> Orderbook<'a> {
                 self.sell_limit_orders.insert(index.try_into().unwrap(), queue);
             }
 
-            return Ok((new_order_id, MatchingSignal::NoOperation));
+            return Ok((new_order_id, if index == 0 { MatchingSignal::NewLowestAsk } else { MatchingSignal::NoOperation }));
         }
 
-        if is_buy_order {
-            self.buy_at_market_orders.push_back(new_order_id);
-            Ok((new_order_id, MatchingSignal::BuyAtMarket))
-        } else {
-            self.sell_at_market_orders.push_back(new_order_id);
-            Ok((new_order_id, MatchingSignal::SellAtMarket))
-        }
+        Ok((new_order_id, if is_buy_order { MatchingSignal::BuyAtMarket } else { MatchingSignal::SellAtMarket }))
     }
 
     pub fn cancel_order(&mut self, order_id: i64) -> Result<(), String> {
@@ -130,33 +124,74 @@ impl <'a> Orderbook<'a> {
         
     }
 
-    pub fn place_order(&mut self, order: Order<'a>) -> Result<i64, String> {
-        match self.insert_order(order) {
-            Ok((order_id, MatchingSignal::NoOperation)) => Ok(order_id),
+    pub fn place_order(&mut self, mut order: Order<'a>) -> Result<i64, String> {
+        match self.insert_order(&mut order) {
             Ok((order_id, matching_signal)) => {
-                self.start_execution_round(matching_signal);
+                match matching_signal {
+                    MatchingSignal::BuyAtMarket => {
+                        // try to match order directly
+                        self.match_against_sell_side_at_market(&mut order);
+                    },
+                    MatchingSignal::SellAtMarket => {
+                        // try to match order directly
+                        self.match_against_buy_side_at_market(&mut order);
+                    },
+                    MatchingSignal::NewHighestBid => {
+                        // try to match any orders on the sell side
+                        self.match_against_sell_side(&mut order);
+                    },
+                    MatchingSignal::NewLowestAsk => {
+                        // try to match any orders on the sell side
+                        self.match_against_buy_side(&mut order);
+                    },
+                    MatchingSignal::NoOperation => {
+                        self.order_map.insert(order_id, order);
+                        // No further actions
+                    },
+                }
+            
                 return Ok(order_id)
             },
             Err(error_text) => Err(error_text),
         }
     }
 
-    fn start_execution_round(&mut self, matching_signal: MatchingSignal) -> Result<Vec<Execution>, String> {
-        match matching_signal {
-            MatchingSignal::BuyAtMarket => Ok(vec![]),  
-            MatchingSignal::NewLowestBid => Ok(vec![]), 
-            MatchingSignal::SellAtMarket => Ok(vec![]),
-            MatchingSignal::NewHighestAsk => Ok(vec![]),
-            MatchingSignal::NoOperation => Ok(vec![]),
-        }
+    fn match_against_buy_side_at_market(&mut self, order: &mut Order<'a>) {
+        // move to order map if no matching is possible or the order is not fully executed
+        // first match with limit orders in order of the price and queue location on the buy side
+
+        // match order and send to the accounting module
+    }
+
+    fn match_against_sell_side_at_market(&mut self, order: &mut Order<'a>) {
+        // move to order map if no matching is possible or the order is not fully executed
+        // first match with limit orders in order of the price and queue location on the sell side
+
+        // match order and send to the accounting module
+    }
+
+    fn match_against_buy_side(&mut self, order: &mut Order<'a>) {
+        // move to order map if no matching is possible or the order is not fully executed
+        // first match with at markets order in order of the queue location on the buy side
+        // then match with limit orders which are higher or equal than this one
+
+        // match order and send to the accounting module
+    }
+
+    fn match_against_sell_side(&mut self, order: &mut Order<'a>) {
+        // move to order map if no matching is possible or the order is not fully executed
+        // first match with market order in order of the queue location on the sell side
+        // then match with limit orders which are lower or equal price than this one
+
+        // match order and send to the accounting module 
     }
 }
 
 enum MatchingSignal {
     BuyAtMarket,
-    NewLowestBid,
     SellAtMarket,
-    NewHighestAsk,
+    NewHighestBid,
+    NewLowestAsk,
     NoOperation
 }
 
